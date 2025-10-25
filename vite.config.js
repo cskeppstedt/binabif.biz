58e9aa4c88daf514e87bf16218fb4f640078f024
Christoffer <c.skeppstedt@gmail.com>
2025-10-25 18:58:18 +0200

    vendor chunk

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,15 @@ export default defineConfig({
         entryFileNames: 'assets/[name]-[hash].js',
         chunkFileNames: 'assets/[name]-[hash].js',
         assetFileNames: 'assets/[name]-[hash][extname]',
+
+        // Manual chunk splitting - separate vendor from app code
+        manualChunks: (id) => {
+          // Put all node_modules code into vendor chunk
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+          // Everything else goes into the default chunk
+        },
       },
     },
   },
